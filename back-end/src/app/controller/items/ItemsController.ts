7fe9adc258c105513e7fc6940d8318340831119d
Rafiou Sitou <rafiousitou90@gmail.com>
2020-06-10 13:44:30 -0300

    feat(multer): Creation of the back-end adding multer file uploader.

--- back-end/src/app/controller/items/ItemsController.ts
+++ back-end/src/app/controller/items/ItemsController.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from 'express'
 import DBConnection from '../../config/connection'
 
-
 class ItemsController {
     async index (req: Request, res: Response) {
         const items = await DBConnection.table('items').select('*').orderBy('title');
 
-        let serializeItems = items.map(item => {
+        const serializeItems = items.map(item => {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://192.168.0.10:3333/uploads/${item.image}`
+                image_url:
+                    `${process.env.APP_SERVER}:${process.env.APP_PORT}/${process.env.APP_UPLOADS_DIR}/${item.image}`
             };
         });
 
--- back-end/src/app/routes/points/index.ts
+++ back-end/src/app/routes/points/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import multer from 'multer';
+
+import multerConfig from '../../config/multer';
 import PointsController from "../../controller/points/PointsController";
 
 const pointsRoutes = express.Router();
 const pointsController = new PointsController();
+const upload = multer(multerConfig);
 
-pointsRoutes.post('/', pointsController.create);
+pointsRoutes.post('/', upload.single('image'), pointsController.create);
 pointsRoutes.get('/:id', pointsController.show);
 pointsRoutes.get('/', pointsController.index);
 
