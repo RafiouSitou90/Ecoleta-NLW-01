fca127e88432c533f01bae489649f4bd38a4ef52
Rafiou Sitou <rafiousitou90@gmail.com>
2020-06-11 14:13:06 -0300

    feat(): Creation of the Home and Point page.  - CreatePoint  - Home

---
+++ front-end/src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
+const Home = () => {
+    return (
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt="Ecoleta"/>
+                </header>
+
+                <main>
+                    <h1>Your waste collection marketplace</h1>
+                    <p>Helps people find collection points efficiently</p>
+
+                    <Link to="/create-point">
+                        <span>
+                            <FiLogIn />
+                        </span>
+                        <strong>register a collection point</strong>
+                    </Link>
+                </main>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
